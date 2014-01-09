eff6925a58f751c889b34c6bf37730d9eb48e4d2
Julien Poulin <poulin_julien@hotmail.com>
2014-01-09 20:11:58 +0100

    Added missing PowerState enumeration.

---
+++ src/Hibernator/Models/PowerState.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PowerState.cs" company="WebForAll">
+//   Copyright © WebForAll. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Julien Poulin</author>
+// <date>09/01/2014</date>
+// <project>Hibernator</project>
+// <web>http://www.webforall.be</web>
+// -----------------------------------------------------------------------
+
+namespace Hibernator.Models
+{
+
+  public enum PowerState
+  {
+    Suspend,
+    Hibernate,
+    Shutdown,
+  }
+
+}
